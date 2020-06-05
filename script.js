010b20535111606e6d6001dacd8a32d2b801a201
Nimitpamnani <C0764944@mylambton.ca>
2020-06-05 11:25:28 -0400

    withsolvedproblem

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
-console.log("happiest")
 
-var date = new Date()
-
-
+//REPORT on the SUM of all numbers from 1000 going down to 17Count down by 11 each timeADD UP all the numbers you step over 
+//as you are counting down from 1000, going down 11 each time, until you get to 17Output the result
 
 
+var val = 0;
+  var i = 1000;
+	for (i; i > 17; i--) {
+  		val += i;
+        i -= 11;
+	}
 
-for (var i = 1000; i >= 0;) {
-
-console.log(i)
-i = i - 17
-
-}
- 
+		document.write(val)
